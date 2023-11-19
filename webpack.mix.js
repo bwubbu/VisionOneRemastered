8b2e1247f888a0b9d8c429ff82f70b85c3e0a94f
SirXise <ninjadororo25@gmail.com>
2023-11-19 11:02:45 +0800

    ////

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,9 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
-    .css('source/_assets/css/main.css', 'css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
+    .sass('source/_assets/sass/main.scss', 'css') // Change to Sass for Bootstrap
     .options({
         processCssUrls: false,
     })
+    .copy('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'source/assets/build/js') // Copy Bootstrap JS
     .version();
