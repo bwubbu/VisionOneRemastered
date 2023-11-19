5591290a9f8fae8652eb638c09233bfc964dab87
SirXise <ninjadororo25@gmail.com>
2023-11-19 15:36:13 +0800

    ..

---
+++ source/_layouts/footer.blade.php
@@ -0,0 +1,64 @@
+<footer>
+
+    <div class="row">
+
+        <div class="six columns tab-whole footer-about">
+
+            <h3>About Puremedia</h3>
+
+            <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+                Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
+                nibh id elit.
+            </p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.</p>
+
+        </div> <!-- /footer-about -->
+
+        <div class="six columns tab-whole right-cols">
+
+            <div class="row">
+
+                <div class="columns">
+                    <h3 class="address">Contact Us</h3>
+                    <p>
+                        1600 Amphitheatre Parkway<br>
+                        Mountain View, CA<br>
+                        94043 US
+                    </p>
+
+                    <ul>
+                        <li><a href="tel:6473438234">647.343.8234</a></li>
+                        <li><a href="tel:1234567890">123.456.7890</a></li>
+                        <li><a href="mailto:someone@puremedia.com">someone@puremedia.com</a></li>
+                    </ul>
+                </div> <!-- /columns -->
+
+                <div class="columns last">
+                    <h3 class="contact">Follow Us</h3>
+
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">GooglePlus</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Flickr</a></li>
+                        <li><a href="#">Skype</a></li>
+                    </ul>
+
+                </div> <!-- /columns -->
+
+            </div> <!-- /Row(nested) -->
+
+        </div>
+
+        <p class="copyright">&copy; Copyright 2014 Puremedia. Design by <a href="http://www.styleshout.com/">Styleshout.</a></p>
+
+        <div id="go-top">
+            <a class="smoothscroll" title="Back to Top" href="#hero"><span>Top</span><i class="fa fa-long-arrow-up"></i></a>
+        </div>
+
+    </div> <!-- /row -->
+
+</footer> <!-- /footer -->
--- source/_layouts/navbar.blade.php
+++ source/_layouts/navbar.blade.php
@@ -1,19 +1,34 @@
-<nav class="navbar sticky-top navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Logo</a>
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/about">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/service">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/project">Projects</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact Us</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <!-- Header
+   =================================================== -->
+    <header id="main-header">
+
+        <div class="row header-inner">
+
+            <div class="logo">
+                <a class="smoothscroll" href="#hero">Puremedia.</a>
+            </div>
+
+            <nav id="nav-wrap">
+
+                <a class="mobile-btn" href="#nav-wrap" title="Show navigation">
+                    <span class='menu-text'>Show Menu</span>
+                    <span class="menu-icon"></span>
+                </a>
+                <a class="mobile-btn" href="#" title="Hide navigation">
+                    <span class='menu-text'>Hide Menu</span>
+                    <span class="menu-icon"></span>
+                </a>
+
+                <ul id="nav" class="nav">
+                    <li class="current"><a class="smoothscroll" href="#hero">Home.</a></li>
+                    <li><a class="smoothscroll" href="#about">About.</a></li>
+                    <li><a class="smoothscroll" href="#services">Services.</a></li>
+                    <li><a class="smoothscroll" href="#portfolio">Projects.</a></li>
+                    <li><a class="smoothscroll" href="#contact">Contact.</a></li>
+                </ul>
+
+            </nav> <!-- /nav-wrap -->
+
+        </div> <!-- /header-inner -->
+
+    </header>
