6cd76590a51190273ee4abe5b264433b8db40bea
SirXise <ninjadororo25@gmail.com>
2023-11-20 23:20:35 +0800

    dsasad

--- source/about.blade.php
+++ source/about.blade.php
@@ -42,28 +42,30 @@
             </div>
         </div>
     </div>
-    <!-- About End -->
+</div>
+<!-- About End -->
 
-
-    <!-- Team Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 1000px;">
-                <h1 class="text-uppercase">Our Offices</h1>
-            </div>
-            <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+<!-- Office -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 1000px;">
+            <h1 class="text-uppercase">Our Offices</h1>
+        </div>
+        <div class="row g-4">
+            <div class="d-flex justify-content-center align-items-stretch" style="gap: px;">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s" style="margin-right: auto; margin-left: auto;">
                     <div class="team-item">
                         <div>
                             <img class="img-fluid" src="assets/build/img/About1.jpeg" alt="">
                         </div>
                         <div class="bg-secondary text-center p-4">
                             <h5 class="text-uppercase">Head Office</h5>
+                            <br>
                             <span class="text-primary">Mutiara Damansara, Petaling Jaya</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.8s">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.8s" style="margin-right: auto; margin-left: auto;">
                     <div class="team-item">
                         <div>
                             <img class="img-fluid" src="assets/build/img/kuantan_branch.jpg" alt="">
@@ -75,26 +77,27 @@
                     </div>
                 </div>
             </div>
-            <div class="row g-4">
-                <div class="col-md-6">
-                    <h3 class="text-uppercase mb-3">Vision</h3>
-                    <p>To be the leader, bridge the gap between conventional and innovative solutions.</p>
-                    <details>
-                        <summary>Show Vision</summary>
-                        <p>Vision One Sdn. Bhd. committed to the mission of upholding the value of customer convictions and services proficiency detailed with precision. Moreover, to substantiate the company’s capability to provide technical solution, inspection, nondestructive testing, maintenance and repair to private and public entities within energy, power and petrochemical industries by providing a services with the assurance of quality and integrity. Breaking through from conventional and advanced technology development in equipment, facilities and personnel expertise.</p>
-                        <p>To implement and to maintain continuous improvement of the quality system to provide efficient services utilizing its resources, infrastructures, and competent, qualified, and experienced personnel to achieve the customer expectation and satisfaction.S</p>
-                    </details>
-                </div>
-                <div class="col-md-6">
-                    <h3 class="text-uppercase mb-3">Mission</h3>
-                    <p>Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
-                    <details>
-                        <summary>Show Mission</summary>
-                        <p>Vision One Sdn. Bhd. aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
-                    </details>
-                </div>
+        </div>
+        <div class="row g-4" style="padding-top: 2.1rem;">
+            <div class="col-md-6">
+                <h3 class="text-uppercase mb-3">Vision</h3>
+                <p>To be the leader, bridge the gap between conventional and innovative solutions.</p>
+                <details>
+                    <summary>Show Vision</summary>
+                    <p>Vision One Sdn. Bhd. committed to the mission of upholding the value of customer convictions and services proficiency detailed with precision. Moreover, to substantiate the company’s capability to provide technical solution, inspection, nondestructive testing, maintenance and repair to private and public entities within energy, power and petrochemical industries by providing a services with the assurance of quality and integrity. Breaking through from conventional and advanced technology development in equipment, facilities and personnel expertise.</p>
+                    <p>To implement and to maintain continuous improvement of the quality system to provide efficient services utilizing its resources, infrastructures, and competent, qualified, and experienced personnel to achieve the customer expectation and satisfaction.S</p>
+                </details>
+            </div>
+            <div class="col-md-6">
+                <h3 class="text-uppercase mb-3">Mission</h3>
+                <p>Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                <details>
+                    <summary>Show Mission</summary>
+                    <p>Vision One Sdn. Bhd. aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
+                </details>
             </div>
         </div>
     </div>
-    <!-- Team End -->
-    @endsection
+</div>
+<!-- Team End -->
+@endsection
