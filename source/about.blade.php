2ecd1f63f0c0c59d3342eba6b6418dd876c35463
bwubbu <kyrodahero123@gmail.com>
2023-11-20 19:48:11 +0800

    uwu lil fixes hehehe

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,25 +1,6 @@
 @extends('_layouts.main')
 
 @section('body')
-<!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s" style="height:4rem">
-    <a href="/" class="navbar-brand ms-4 ms-lg-0">
-        <img width="300" height="67" src="https://visionone.com.my/wp-content/uploads/2015/07/logo-300x67.png" alt="Logo" />
-    </a>
-    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="/" class="nav-item nav-link">Home</a>
-            <a href="/about" class="nav-item nav-link active">About</a>
-            <a href="/service" class="nav-item nav-link">Service</a>
-            <a href="/project" class="nav-item nav-link">Project</a>
-            <a href="/contact" class="nav-item nav-link">Contact</a>
-        </div>
-    </div>
-</nav>
-<!-- Navbar End -->
 
 <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
@@ -33,7 +14,7 @@
 <!-- About Start -->
 <div class="container-xxl">
     <div class="container">
-        <div class="col-lg-7 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
+        <div class="col-lg-10 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
             <h1 class="text-uppercase mb-4">To be the leader, bridge the gap between conventional and innovative solutions.</h1>
             <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
             <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
@@ -51,7 +32,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-7">
+        <div class="col-lg-6">
             <div class="col-md-10 wow fadeIn" data-wow-delay="0.1s">
                 <div class="d-flex flex-column py-5">
                     <img class="img-fluid w-50 align-left" src="assets/build/img/About1.jpeg" alt="">
@@ -61,7 +42,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 20%; transform: translateX(-20%); text-align: center;">
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 35%; transform: translateX(-35%); text-align: center;">
                 <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
                 <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
                 <p class="mb-4">Vision One Sdn. Bhd. together with its associate companies will form collaborative arrangements to achieve its business targets. The capacity, expertise and experiences within the group provide VOSB and its associate a competitive service advantage.</p>
