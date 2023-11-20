36e0bd874ee8e8b6c8bbcaada1fbf9b8da02716e
bwubbu <kyrodahero123@gmail.com>
2023-11-20 18:23:52 +0800

    lol!

--- source/about.blade.php
+++ source/about.blade.php
@@ -31,34 +31,33 @@
 
 
 <!-- About Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-5">
-                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="d-flex flex-column">
-                        <img class="img-fluid w-75 align-self-end" src="assets/build/img/About1.jpeg" alt="">
-                        <div class="w-50 bg-secondary p-5" style="margin-top: -25%;">
-                            <h1 class="text-uppercase text-primary mb-3">Head Office</h1>
-                            <h2 class="text-uppercase mb-0">Mutiara Damansara, Petaling Jaya</h2>
-                        </div>
-                    </div>
+<div class="container-xxl">
+    <div class="container">
+        <div class="col-lg-7 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
+            <h1 class="text-uppercase mb-4">To be the leader, bridge the gap between conventional and innovative solutions.</h1>
+            <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
+            <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
+            <p class="mb-4">Vision One Sdn. Bhd. together with its associate companies will form collaborative arrangements to achieve its business targets. The capacity, expertise and experiences within the group provide VOSB and its associate a competitive service advantage.</p>
+            <p class="mb-4">Vision One Sdn. Bhd. meticulous inclination and positioning will be to appeal to the private and public entities the value of professional services from experienced personnel who are committed to carry out projects objectively and technically in accordance with the international standards and code of practice to ensure its viability and delivery
+                as per requirements to meet customers’ expectations. The utilization of state-of-the-art equipment and software solutions on every project will enhance customers’ ability to meet its operation requirements.</p>
+            <div class="row g-4">
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Vision</h3>
+                    <p class="mb-0">Vision One Sdn. Bhd.'s aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
                 </div>
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                <p class="d-inline-block bg-secondary text-primary py-1 px-4">About Us</p>
-                <h1 class="text-uppercase mb-4">To be the leader, bridge the gap between conventional and innovative solutions.</h1>
-                <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
-                <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
-                <p class="mb-4">Vision One Sdn. Bhd. together with its associate companies will form collaborative arrangements to achieve its business targets. The capacity, expertise and experiences within the group provide VOSB and its associate a competitive service advantage.</p>
-                <p class="mb-4">Vision One Sdn. Bhd. meticulous inclination and positioning will be to appeal to the private and public entities the value of professional services from experienced personnel who are committed to carry out projects objectively and technically in accordance with the international standards and code of practice to ensure its viability and delivery 
-                                as per requirements to meet customers’ expectations. The utilization of state-of-the-art equipment and software solutions on every project will enhance customers’ ability to meet its operation requirements.</p>
-                <div class="row g-4">
-                    <div class="col-md-6">
-                        <h3 class="text-uppercase mb-3">Vision</h3>
-                        <p class="mb-0">Vision One Sdn. Bhd.'s aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <h3 class="text-uppercase mb-3">Mission</h3>
-                        <p class="mb-0">Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Mission</h3>
+                    <p class="mb-0">Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                </div>
+            </div>
+        </div>
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                <div class="d-flex flex-column">
+                    <img class="img-fluid w-75 align-self-end" src="assets/build/img/About1.jpeg" alt="">
+                    <div class="w-50 bg-secondary p-5" style="margin-top: -25%;">
+                        <h3 class="text-uppercase text-primary mb-3">Head Office</h3>
+                        <h4 class="text-uppercase mb-0">Mutiara Damansara, Petaling Jaya</h4>
                     </div>
                 </div>
             </div>
