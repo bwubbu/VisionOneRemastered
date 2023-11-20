6fff63603f2080c681a7641f8186cce6557eb88c
bwubbu <kyrodahero123@gmail.com>
2023-11-20 22:51:15 +0800

    lol!

--- source/about.blade.php
+++ source/about.blade.php
@@ -14,7 +14,7 @@
 <!-- About Start -->
 <div class="container-xxl">
     <div class="container">
-        <div class="col-lg-10 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
+        <div class="col-lg-9 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
             <h1 class="text-uppercase mb-4">To be the leader, bridge the gap between conventional and innovative solutions.</h1>
             <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
             <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
@@ -24,30 +24,21 @@
             <div class="row g-4">
                 <div class="col-md-6">
                     <h3 class="text-uppercase mb-3">Vision</h3>
-                    <p class="mb-0">Vision One Sdn. Bhd.'s aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
+                    <p>To be the leader, bridge the gap between conventional and innovative solutions.</p>
+                    <details>
+                        <summary>Show Vision</summary>
+                        <p>Vision One Sdn. Bhd. committed to the mission of upholding the value of customer convictions and services proficiency detailed with precision. Moreover, to substantiate the company’s capability to provide technical solution, inspection, nondestructive testing, maintenance and repair to private and public entities within energy, power and petrochemical industries by providing a services with the assurance of quality and integrity. Breaking through from conventional and advanced technology development in equipment, facilities and personnel expertise.</p>
+                        <p>To implement and to maintain continuous improvement of the quality system to provide efficient services utilizing its resources, infrastructures, and competent, qualified, and experienced personnel to achieve the customer expectation and satisfaction.S</p>
+                    </details>
                 </div>
                 <div class="col-md-6">
                     <h3 class="text-uppercase mb-3">Mission</h3>
-                    <p class="mb-0">Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                    <p>Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                    <details>
+                        <summary>Show Mission</summary>
+                        <p>Vision One Sdn. Bhd. aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
+                    </details>
                 </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="col-md-10 wow fadeIn" data-wow-delay="0.1s">
-                <div class="d-flex flex-column py-5">
-                    <img class="img-fluid w-50 align-left" src="assets/build/img/About1.jpeg" alt="">
-                    <div class="bg-secondary text-center p-4 w-50">
-                        <h5 class="text-uppercase">Head Office</h5>
-                        <span class="text-primary">Mutiara Damansara, Petaling Jaya</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 35%; transform: translateX(-35%); text-align: center;">
-                <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
-                <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
-                <p class="mb-4">Vision One Sdn. Bhd. together with its associate companies will form collaborative arrangements to achieve its business targets. The capacity, expertise and experiences within the group provide VOSB and its associate a competitive service advantage.</p>
-                <p class="mb-4">Vision One Sdn. Bhd. meticulous inclination and positioning will be to appeal to the private and public entities the value of professional services from experienced personnel who are committed to carry out projects objectively and technically in accordance with the international standards and code of practice to ensure its viability and delivery
-                    as per requirements to meet customers’ expectations. The utilization of state-of-the-art equipment and software solutions on every project will enhance customers’ ability to meet its operation requirements.</p>
             </div>
         </div>
     </div>
@@ -57,74 +48,50 @@
     <!-- Team Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <p class="d-inline-block bg-secondary text-primary py-1 px-4">Our Barber</p>
-                <h1 class="text-uppercase">Meet Our Barber</h1>
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 1000px;">
+                <h1 class="text-uppercase">Our Offices</h1>
             </div>
             <div class="row g-4">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="team-item">
-                        <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-1.jpg" alt="">
-                            <div class="team-social">
-                                <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                        <div class="bg-secondary text-center p-4">
-                            <h5 class="text-uppercase">Barber Name</h5>
-                            <span class="text-primary">Designation</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="team-item">
-                        <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-2.jpg" alt="">
-                            <div class="team-social">
-                                <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                        <div class="bg-secondary text-center p-4">
-                            <h5 class="text-uppercase">Barber Name</h5>
-                            <span class="text-primary">Designation</span>
-                        </div>
-                    </div>
-                </div>
                 <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="team-item">
-                        <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-3.jpg" alt="">
-                            <div class="team-social">
-                                <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                        <div>
+                            <img class="img-fluid" src="assets/build/img/About1.jpeg" alt="">
                         </div>
                         <div class="bg-secondary text-center p-4">
-                            <h5 class="text-uppercase">Barber Name</h5>
-                            <span class="text-primary">Designation</span>
+                            <h5 class="text-uppercase">Head Office</h5>
+                            <span class="text-primary">Mutiara Damansara, Petaling Jaya</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.8s">
                     <div class="team-item">
-                        <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid" src="img/team-4.jpg" alt="">
-                            <div class="team-social">
-                                <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
+                        <div>
+                            <img class="img-fluid" src="assets/build/img/kuantan_branch.jpg" alt="">
                         </div>
                         <div class="bg-secondary text-center p-4">
-                            <h5 class="text-uppercase">Barber Name</h5>
-                            <span class="text-primary">Designation</span>
+                            <h5 class="text-uppercase">Kuantan Branch Office and Yard Facility</h5>
+                            <span class="text-primary">Gebeng, Kuantan</span>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Vision</h3>
+                    <p>To be the leader, bridge the gap between conventional and innovative solutions.</p>
+                    <details>
+                        <summary>Show Vision</summary>
+                        <p>Vision One Sdn. Bhd. committed to the mission of upholding the value of customer convictions and services proficiency detailed with precision. Moreover, to substantiate the company’s capability to provide technical solution, inspection, nondestructive testing, maintenance and repair to private and public entities within energy, power and petrochemical industries by providing a services with the assurance of quality and integrity. Breaking through from conventional and advanced technology development in equipment, facilities and personnel expertise.</p>
+                        <p>To implement and to maintain continuous improvement of the quality system to provide efficient services utilizing its resources, infrastructures, and competent, qualified, and experienced personnel to achieve the customer expectation and satisfaction.S</p>
+                    </details>
+                </div>
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Mission</h3>
+                    <p>Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                    <details>
+                        <summary>Show Mission</summary>
+                        <p>Vision One Sdn. Bhd. aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
+                    </details>
                 </div>
             </div>
         </div>
