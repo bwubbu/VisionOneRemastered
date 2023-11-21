99263d2843f67aa06a6b3784052bdc19d14b3ce2
SirXise <ninjadororo25@gmail.com>
2023-11-21 21:01:00 +0800

    ....

--- source/project.blade.php
+++ source/project.blade.php
@@ -1,6 +1,49 @@
 @extends('_layouts.main')
 
 @section('body')
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container text-center py-5">
+        <h1 class="display-3 text-white text-uppercase mb-3 animated slideInDown">Projects</h1>
+    </div>
+</div>
+<!-- Page Header End -->
+
+<!-- Project Words Start -->
+<div class="container-xxl">
+    <div class="container">
+        <div class="col-lg-9 wow fadeIn" data-wow-delay="0.5s" style="position: relative; left: 50%; transform: translateX(-50%); text-align: center;">
+            <h1 class="text-uppercase mb-4">To be the leader, bridge the gap between conventional and innovative solutions.</h1>
+            <p>Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair. These provisions of services adhere to the international standards in state-of –the-art and conventional methods with the goal in delivering the work with quality and utmost integrity to meet customer satisfaction.</p>
+            <p class="mb-4">The company is registered and accredited by national and international certifying bodies. The company composed of congregates of highly competent, certified and qualified personnel in specific fields of services.</p>
+            <p class="mb-4">Vision One Sdn. Bhd. together with its associate companies will form collaborative arrangements to achieve its business targets. The capacity, expertise and experiences within the group provide VOSB and its associate a competitive service advantage.</p>
+            <p class="mb-4">Vision One Sdn. Bhd. meticulous inclination and positioning will be to appeal to the private and public entities the value of professional services from experienced personnel who are committed to carry out projects objectively and technically in accordance with the international standards and code of practice to ensure its viability and delivery
+                as per requirements to meet customers’ expectations. The utilization of state-of-the-art equipment and software solutions on every project will enhance customers’ ability to meet its operation requirements.</p>
+            <div class="row g-4">
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Vision</h3>
+                    <p>To be the leader, bridge the gap between conventional and innovative solutions.</p>
+                    <details>
+                        <summary>Show Vision</summary>
+                        <p>Vision One Sdn. Bhd. committed to the mission of upholding the value of customer convictions and services proficiency detailed with precision. Moreover, to substantiate the company’s capability to provide technical solution, inspection, nondestructive testing, maintenance and repair to private and public entities within energy, power and petrochemical industries by providing a services with the assurance of quality and integrity. Breaking through from conventional and advanced technology development in equipment, facilities and personnel expertise.</p>
+                        <p>To implement and to maintain continuous improvement of the quality system to provide efficient services utilizing its resources, infrastructures, and competent, qualified, and experienced personnel to achieve the customer expectation and satisfaction.S</p>
+                    </details>
+                </div>
+                <div class="col-md-6">
+                    <h3 class="text-uppercase mb-3">Mission</h3>
+                    <p>Pursue, acquire, introduce and drive innovative, advanced solutions into the energy, power and petrochemical industries</p>
+                    <details>
+                        <summary>Show Mission</summary>
+                        <p>Vision One Sdn. Bhd. aspiration to be the national and international leading provider of technical solution, inspection, nondestructive testing, maintenance and repair to the respected customers in energy, power, and petrochemical industries.</p>
+                    </details>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Project Words End -->
+
+<!-- Project Start -->
 <div class="container my-5">
     <div class="card card-primary">
         <div class="card-header" style="border-radius: 0.25rem 0.25rem 0 0 !important;">
@@ -642,11 +685,10 @@
             <table id="example2" class="table table-bordered table-hover">
                 <thead>
                     <tr>
-                        <th>Rendering engine</th>
-                        <th>Browser</th>
-                        <th>Platform(s)</th>
-                        <th>Engine version</th>
-                        <th>CSS grade</th>
+                        <th>No.</th>
+                        <th>Customer</th>
+                        <th>Project Description</th>
+                        <th>Year</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -1006,6 +1048,7 @@
         <!-- /.card-body -->
     </div>
 </div>
+<!-- Project End -->
 @endsection
 
 @section('scripts')
