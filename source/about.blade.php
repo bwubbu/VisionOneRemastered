e8db481b33ac6929e84df6189bf90d2d87862f47
bwubbu <kyrodahero123@gmail.com>
2023-11-24 00:06:56 +0800

    last

--- source/about.blade.php
+++ source/about.blade.php
@@ -29,14 +29,14 @@
             <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.5s">
                 <img class="img-fluid" src="assets/build/img/MaintenancePic.jpg" alt="Image">
             </div>
-            <div class="col-lg-6 wow fadeInRight justify-content-center" data-wow-delay="0.5s">
+            <div class="col-lg-6 wow fadeInRight d-flex align-items-center" data-wow-delay="0.5s">
                 <p class="mb-4" style="font-size:20px">Vision One Sdn. Bhd. is a company that focuses on the provision of technical solution, inspection, nondestructive testing, maintenance and repair.
                     These provisions of services adhere to the international standards in state-of-the-art and conventional methods with the goal of delivering the work with quality
                     and utmost integrity to meet customer satisfaction.</p>
             </div>
         </div>
         <div class="row justify-content-center" style="padding-top: 20px; padding-bottom: 50px;">
-            <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.5s">
+            <div class="col-lg-6 wow fadeInLeft d-flex align-items-center" data-wow-delay="0.5s">
                 <p class="mb-4" style="font-size:20px">The company is registered and accredited by national and international certifying bodies. The company composed of congregates
                     of highly competent, certified and qualified personnel in specific fields of services.</p>
             </div>
@@ -48,11 +48,19 @@
             <div class="col-lg-6 wow fadeInLeft" data-wow-delay="0.5s">
                 <img class="img-fluid" src="assets/build/img/Collaboration.jpg" alt="Image">
             </div>
-            <div class="col-lg-6 wow fadeInRight justify-content-center" data-wow-delay="0.5s">
+            <div class="col-lg-6 wow fadeInRight d-flex align-items-center" data-wow-delay="0.5s">
                 <p class="mb-4" style="font-size:20px">Vision One Sdn. Bhd. together with its associate companies will form collaborative
                     arrangements to achieve its business targets. The capacity, expertise and experiences within the
                     group provide VOSB and its associate a competitive service advantage.</p>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow bounceIn" data-wow-delay="0.3s" style="max-width: 1000px;">
+            <h1 class="text-uppercase">Our Offices</h1>
         </div>
     </div>
 </div>
@@ -71,8 +79,8 @@
                             </div>
                         </div>
                         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                            <div>
-                                <img class="img-fluid" style="object-fit: cover;" src="assets/build/img/About1.jpeg" alt="">
+                            <div style="width: 100%; height: 100%;">
+                                <img class="img-fluid" style="object-fit: cover; width: 100%; height: 100%;" src="assets/build/img/About1.jpeg" alt="">
                             </div>
                         </div>
                     </div>
@@ -83,13 +91,13 @@
                     <div class="row g-0">
                         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
                             <div class="bg-secondary d-flex flex-column justify-content-center p-5">
-                                <h3 class="text-uppercase mb-4">Kuantan Branch Office and Yard Facility</h3>
-                                <p style="color: white; text-align: justify;">Mutiara Damansara, Petaling Jaya</p>
+                                <h4 class="text-uppercase mb-4">Kuantan Branch Office and Yard Facility</h4>
+                                <p style="color: white; text-align: justify;">Gebeng, Kuantan</p>
                             </div>
                         </div>
                         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                            <div>
-                                <img class="img-fluid" style="object-fit: cover;" src="assets/build/img/kuantan_branch.jpg" alt="">
+                            <div style="width: 100%; height: 100%;">
+                                <img class="img-fluid" style="object-fit: cover; width: 100%; height: 100%;" src="assets/build/img/kuantan_branch.jpg" alt="">
                             </div>
                         </div>
                     </div>
